c37e54741f639af3650b73992e6b893fa05dd046
senjit02 <m.senjit@gmail.com>
2019-05-23 02:24:26 +1000

    initial commit

---
+++ assets/Config/flag.js
@@ -0,0 +1,11 @@
+window.flag = {
+    allEventsCardRoundedImage: true      // true / false
+};
+
+
+window.urls = {
+    homeSliderData: "assets/Config/showcase-slider-data.json",
+    highlightedEventsData: "assets/Config/highlighted-events.json",
+    committeeMembersData: "assets/Config/committee-member.json"
+}
+
