154c849d5ad0b661d0aaaf9f0b012bc2d88ba124
senjit02 <m.senjit@gmail.com>
2019-05-25 14:57:46 +1000

    added about us banner

---
+++ assets/Config/flag.js
@@ -0,0 +1,13 @@
+window.flag = {
+    allEventsCardRoundedImage: false      // true / false
+};
+
+
+window.urls = {
+    eventsData: "assets/Config/all-events.json",
+    homeSliderData: "assets/Config/showcase-slider-data.json",
+    highlightedEventsData: "assets/Config/highlighted-events.json",
+    committeeMembersData: "assets/Config/committee-member.json",
+	galaryData: "assets/Config/galary.json"
+}
+
